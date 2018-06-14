30c81cfea32b266360c023ba5d51144cc7132550
Omar Voegele <omar@mindliner.com>
2018-06-14 20:59:15 +0200

    initialer commit

---
+++ JS/script01.js
@@ -0,0 +1,13 @@
+function addTableTask() {
+  var tableBody = document.getElementById('body-task-table')[0];
+
+  var tableRow = document.createElement("tr");
+  var classArray = ["state", "task", "deadline", "importance", "edit", "delete"];
+  for(var i = 0; i < 6; i++) {
+    console.log(i);
+    var cell = document.createElement("td".className="classArray[i]");
+  }
+
+
+};
+alert("hello World");
